4c49732e8a0df3c6da43d7d50c6d2a4db104a0d0
jose pablo jimenez <jose_pablo_skate@hotmail.com>
2019-08-29 12:29:34 -0500

    Se crean estilos con bottstrap

---
+++ Ejemplo-1/app.js
@@ -0,0 +1,11 @@
+let btnCalcular = document.getElementById('btnCalcular');
+let numero1 = document.getElementById('numero1');
+let numero2 = document.getElementById('numero2');
+let resultado = document.getElementById('resultado');
+
+btnCalcular.addEventListener('click', suma);
+
+function suma {
+    resultado = numero1 + numero2;
+
+}
