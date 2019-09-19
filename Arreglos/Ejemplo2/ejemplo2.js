702ad73af028102ed877962650f0c668a7120634
jose pablo jimenez <jose_pablo_skate@hotmail.com>
2019-09-19 11:57:25 -0500

    Se concluye Ejemplo 2

--- Arreglos/Ejemplo2/ejemplo2.js
+++ Arreglos/Ejemplo2/ejemplo2.js
@@ -6,26 +6,31 @@ evento();
 
 function evento() {
     btnAgregarElementos.addEventListener('click', AgregarElementos);
-    btnMostrarElementos.addEventListener('click', MostrarElementos);
+    btnMostrarElementos.addEventListener('click', CalcularMedia);
 }
 
 function AgregarElementos() {
 
     if (contador < 10) {
         let valorInput = document.getElementById('AgregarElemento');
-        elementos[contador] = valorInput.value;
+        elementos[contador] = Number(valorInput.value);
         valorInput.value = "";
         contador++;
     }
 
 }
 
-function MostrarElementos() {
-    let informacionImprimir = "";
-    for (let i = 0; i < elementos.length; i++) {
-        informacionImprimir += `<li class="list-group-item">${elementos[i]}</li>`
+function CalcularMedia() {
 
+    let promedio = 0;
+
+    for (let i = 0; i < elementos.length; i++) {
+        promedio += elementos[i];
     }
+    promedio /= elementos.length;
     contador = 0;
-    document.getElementById('listaImprimir').innerHTML = informacionImprimir;
+    elementos = [];
+
+    let imprimir = `<li class="list-group-item">El promedio de los números otorgados es ${promedio}</li>`
+    document.getElementById('listaImprimir').innerHTML = imprimir;
 }
